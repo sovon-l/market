093784b4d44eba177dc4e797eb36a42efaf061fc
orb li <mail@orbb.li>
2022-06-10 15:28:23 +0800

    align with restructure of messenger

--- src/api/market_listener.rs
+++ src/api/market_listener.rs
@@ -2,7 +2,7 @@ pub struct MarketListener<S: MarketListenerTrait> {
     pub inner: S,
 }
 
-impl<S: MarketListenerTrait> messenger::subscriber::Listener<crate::message::Message>
+impl<S: MarketListenerTrait> messenger::pubsub::Listener<crate::message::Message>
     for MarketListener<S>
 {
     fn on_msg(&mut self, msg: crate::message::Message) {
@@ -17,14 +17,3 @@ pub trait MarketListenerTrait {
     fn on_bbo(&mut self, msg: crate::structs::market_price::MarketPrice);
     fn on_trades(&mut self, msg: crate::structs::trades::Trades);
 }
-
-// below are implementation examples
-
-// 1. zmqsbe subscriber
-pub type ZmqSbeSubscriber = messenger::subscriber::ZmqSubscriber;
-
-// 2. bus subscriber
-pub type BusSubscriber = messenger::subscriber::BusSubscriber<crate::message::Message>;
-
-// 3. crossbeam subscriber
-pub type CrossbeamSubscriber = messenger::subscriber::CrossbeamSubscriber<crate::message::Message>;
