c9bcb778b5b02bff82a73bc0b8d962d812f2a429
orb li <mail@orbb.li>
2022-08-20 11:59:46 +0800

    binance orderbook building algo

--- src/data/exchange/binance/async/orderbook.rs
+++ src/data/exchange/binance/async/orderbook.rs
@@ -30,15 +30,149 @@ impl crate::util::websocket::WssState for State {
     }
 }
 
-struct UpdateSeqNum {
+// seq num facilities
+
+#[derive(Clone, Copy)]
+pub struct UpdateSeqNum {
     first: u64,
     last: u64,
 }
 type MbpUpdate = crate::data::orderbook::MbpUpdate<UpdateSeqNum>;
-struct SnapshotSeqNum {
+#[derive(Clone, Copy)]
+pub struct FullbookSeqNum {
     seq_num: u64,
 }
-type MbpFullbook = crate::data::orderbook::MbpFullbook<SnapshotSeqNum>;
+impl crate::data::orderbook::VerificationStatus for FullbookSeqNum {
+    type VerificationUpdate = UpdateSeqNum;
+    fn verify(
+        orderbook: &[crate::util::orderbook::depth::MbpDepth], 
+        status: &mut Self, 
+        update: &mut Self::VerificationUpdate,
+    ) -> Option<bool> {
+        if status.seq_num < update.first + 1 {
+            return Some(false)
+        }
+        if status.seq_num < update.last {
+            return None
+        }
+        status.seq_num = update.last;
+        Some(true)
+    }
+}
+type MbpFullbook = crate::data::orderbook::MbpFullbook<FullbookSeqNum>;
+
+// cacher
+
+pub enum MbpCache {
+    BuiltBook(MbpFullbook),
+    BuildingBook(Vec<MbpUpdate>),
+}
+impl crate::data::mbp_handler::MbpCache<UpdateSeqNum, FullbookSeqNum> for MbpCache {
+    fn is_book_built(&self) -> bool {
+        match self {
+            MbpCache::BuiltBook(_) => true,
+            MbpCache::BuildingBook(_) => false,
+        }
+    }
+    fn new_response(book: MbpFullbook) -> Self {
+        MbpCache::BuiltBook(book)
+    }
+    fn new_update(update: MbpUpdate) -> Self {
+        MbpCache::BuildingBook(vec![update])
+    }
+    fn process_response(&self, book: MbpFullbook) -> Result<Option<Self>, Box<dyn std::error::Error>> {
+        match self {
+            MbpCache::BuiltBook(b) => {
+                if book.verification_status.seq_num > b.verification_status.seq_num {
+                    log::debug!("refresh book");
+                    Ok(Some(MbpCache::BuiltBook(book)))
+                } else {
+                    log::warn!("unusual old refresh/response received, discarding");
+                    Ok(None)
+                }
+            }
+            MbpCache::BuildingBook(updates) => {
+                let book_seq_num = book.verification_status.seq_num;
+                let update_first_prev_seq_num = &updates[0].verification_update.first;
+                let update_last_seq_num = &updates[updates.len() - 1].verification_update.last;
+
+                if book_seq_num < *update_first_prev_seq_num {
+                    log::debug!("seq num too small, discarding");
+                    return Ok(None);
+                }
+                if *update_last_seq_num <= book_seq_num {
+                    log::debug!("updates are too old, use book directly");
+                    return Ok(Some(Self::BuiltBook(book)));
+                }
+                let mut mbp_book = Self::BuiltBook(book);
+                let mut found = false;
+                for update in updates {
+                    if let Self::BuiltBook(mbp_book) = &mbp_book {
+                        let update_prev_seq_num = update.verification_update.first;
+                        let update_seq_num = update.verification_update.last;
+                        let book_seq_num = mbp_book.verification_status.seq_num;
+
+                        if update_prev_seq_num <= book_seq_num && book_seq_num < update_seq_num {
+                            found = true;
+                        }
+                    }
+                    if found {
+                        log::info!("building book processing cached updates");
+                        mbp_book.process_update(update.clone())?;
+                    }
+                }
+                if !found {
+                    return Err("cache.seq_num < resp.seq_num but not found".into());
+                }
+                Ok(Some(mbp_book))
+            }
+        }
+    }
+    fn process_update(&mut self, update: MbpUpdate) -> Result<(), Box<dyn std::error::Error>> {
+        match self {
+            MbpCache::BuildingBook(u) => {
+                if u.len() > 1000 {
+                    Err("accumulated updates without refresh".into())
+                } else {
+                    u.push(update);
+                    Ok(())
+                }
+            }
+            MbpCache::BuiltBook(b) => {
+                let update_prev_seq_num = update.verification_update.first;
+                let update_seq_num = update.verification_update.last;
+                let book_seq_num = b.verification_status.seq_num;
+
+                if update_seq_num <= book_seq_num {
+                    log::debug!(
+                        "our {} their prev {} their new {} - discard",
+                        book_seq_num, update_prev_seq_num, update_seq_num
+                    );
+                    Ok(())
+                } else if book_seq_num + 1 == update_prev_seq_num {
+                    log::debug!(
+                        "our {} their prev {} their new {} - normal",
+                        book_seq_num, update_prev_seq_num, update_seq_num
+                    );
+                    let _ = b.update(update);
+                    Ok(())
+                } else if update_prev_seq_num <= book_seq_num && book_seq_num < update_seq_num {
+                    log::debug!(
+                        "our {} their prev {} their new {} - catchup",
+                        book_seq_num, update_prev_seq_num, update_seq_num
+                    );
+                    let _ = b.update(update);
+                    Ok(())
+                } else {
+                    log::error!("our {} their prev {} their new {}", book_seq_num, update_prev_seq_num, update_seq_num);
+                    Err("seq num mismatch".into())
+                }
+            }
+        }
+    }
+}
+
+// wss
 
 pub fn wss(
     mut sender: impl messenger::traits::ChannelSender<crate::message::Message>,
@@ -93,6 +227,9 @@ pub fn wss(
                 return None;
             }
         };
+        let WssMessage { data, .. } = msg;
+        let WssData { E, s, U, u, b, a } = data;
+
         // bid < midprice, +ve quantity
         // ask > midprice, -ve quantity
         None
